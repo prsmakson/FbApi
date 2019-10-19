28d5be2f1fd31059f5ce993bee5188edb66d47e8
prsmakson <fen1csw0w@gmail.com>
2019-10-19 20:30:52 +0300

    Ничего

--- FbApi/FbApi/Program.cs
+++ FbApi/FbApi/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using OpenQA.Selenium.Chrome;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace FbApi
 {
+   
     class Program
-    {
-        static void Main(string[] args)
+    { 
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main()
         {
             Console.WriteLine("Hello World!");
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("http://www.droidapps.ga/");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+             
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
         }
     }
 }
