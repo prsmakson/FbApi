01d2bd153fcbf0b9a4e950bf172dd9e2d7e780d4
prsmakson <fen1csw0w@gmail.com>
2019-10-20 14:40:04 +0300

    добавил сущность компания

---
+++ FbApi/Fb.Api/Models/Campaign.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fb.Api.Models
+{
+    class Campaign : BaseModel
+    {
+    }
+    public enum CONFIGURED_STATUS: int
+    {
+
+    }
+}
